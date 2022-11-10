4b9f71d02aba99ac3389117d16cd62132879dd43
Jooeun Park <89099318+Jep4@users.noreply.github.com>
2022-11-09 16:44:28 -0800

    Update tBot.py

--- tBot.py
+++ tBot.py
@@ -1,21 +1,12 @@
-from pathlib import Path
-import os
-import environ
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-env = environ.Env(
-    DEBUG=(bool, False)
-)
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # main code start
 import twitter, time
 
 # twitter api connect
-twitter_consumer_key = env('xE2KeJ6dd98hHSR5XOmPr615K')
-twitter_consumer_secret = env('IDiGtGAgjUePiQcjZH2ODOfkYC2ytwkhbM0Pr5EBwv7qGeyxz9')
-twitter_access_token = env('1589819209575698432-T7I2dl9tmGrlWNQxyyhgzUWnbx5nGn')
-twitter_access_secret = env('YcsAwIs6KUmMUlZXowgKruavACoRIh2mGgU4tT7xrOvQJ')
+twitter_consumer_key = 'xE2KeJ6dd98hHSR5XOmPr615K'
+twitter_consumer_secret = 'IDiGtGAgjUePiQcjZH2ODOfkYC2ytwkhbM0Pr5EBwv7qGeyxz9'
+twitter_access_token = '1589819209575698432-T7I2dl9tmGrlWNQxyyhgzUWnbx5nGn'
+twitter_access_secret = 'YcsAwIs6KUmMUlZXowgKruavACoRIh2mGgU4tT7xrOvQJ'
 
 twitter_api = twitter.Api(consumer_key=twitter_consumer_key,
                           consumer_secret=twitter_consumer_secret, 
