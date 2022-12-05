f766c01d814941a31b4d3c211714df10dd246352
Jooeun Park <89099318+Jep4@users.noreply.github.com>
2022-12-05 14:21:23 -0800

    plot-making code

--- word_ratio.py
+++ word_ratio.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
 df1 = pd.read_csv("data.csv")
@@ -7,6 +6,7 @@ df2 = pd.read_csv("data2.csv")
 df3 = pd.read_csv("data3.csv")
 
 combined = pd.concat([df1, df2, df3])
+
 neut = combined[combined['class'] == 'N']
 repub = combined[combined['class'] == 'R']
 democ = combined[combined['class'] == 'D']
@@ -17,19 +17,36 @@ df = pd.DataFrame()
 df['tweets'] = neut
 df['tweets'] = df['tweets'].str.replace("\n", "")
 df['tweets'] = df['tweets'].str.replace("\"", "")
-df['tweets'] = df['tweets'].str.replace("to", "")
-df['tweets'] = df['tweets'].str.replace("the", "")
-df['tweets'] = df['tweets'].str.replace("a", "")
-df['tweets'] = df['tweets'].str.replace("of", "")
-df['tweets'] = df['tweets'].str.replace("is", "")
+
+
 df = df.groupby(['tweets']).size().reset_index(name="size")
 df = df.sort_values('size', ascending=False)
 
-lists = df.set_index("tweets").to_dict()['size']
+df = df[df['tweets'] != "to"]
+df = df[df['tweets'] != "the"]
+df = df[df['tweets'] != "of"]
+df = df[df['tweets'] != "is"]
+df = df[df['tweets'] != "in"]
+df = df[df['tweets'] != "that"]
+df = df[df['tweets'] != "I"]
+df = df[df['tweets'] != "a"]
+df = df[df['tweets'] != "."]
+df = df[df['tweets'] != "The"]
+df = df[df['tweets'] != "and"]
+df = df[df['tweets'] != "for"]
+df = df[df['tweets'] != "be"]
+df = df[df['tweets'] != "you"]
+df = df[df['tweets'] != "are"]
+df = df[df['tweets'] != "it"]
+df = df[df['tweets'] != "with"]
+df = df[df['tweets'] != ""]
+df = df[df['tweets'] != "ownership."]
+df = df[df['tweets'] != "ownership,"]
+df = df[df['tweets'] != "on"]
 
-wordcloud = WordCloud().generate_from_frequencies(lists)
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
+plt.bar(df['tweets'][1:16], df['size'][1:16])
+plt.title("Word count: Neutral tweets")
+plt.ylabel("frequency")
 plt.show()
 
 repub = repub['tweets'].str.split(' ')
@@ -38,22 +55,36 @@ df = pd.DataFrame()
 df['tweets'] = repub
 df['tweets'] = df['tweets'].str.replace("\n", "")
 df['tweets'] = df['tweets'].str.replace("\"", "")
-df['tweets'] = df['tweets'].str.replace("to", "")
-df['tweets'] = df['tweets'].str.replace("the", "")
-df['tweets'] = df['tweets'].str.replace("a", "")
-df['tweets'] = df['tweets'].str.replace("of", "")
-df['tweets'] = df['tweets'].str.replace("is", "")
+
 df = df.groupby(['tweets']).size().reset_index(name="size")
 df = df.sort_values('size', ascending=False)
 
-lists = df.set_index("tweets").to_dict()['size']
+df = df[df['tweets'] != "to"]
+df = df[df['tweets'] != "the"]
+df = df[df['tweets'] != "of"]
+df = df[df['tweets'] != "is"]
+df = df[df['tweets'] != "in"]
+df = df[df['tweets'] != "that"]
+df = df[df['tweets'] != "I"]
+df = df[df['tweets'] != "a"]
+df = df[df['tweets'] != "."]
+df = df[df['tweets'] != "The"]
+df = df[df['tweets'] != "and"]
+df = df[df['tweets'] != "for"]
+df = df[df['tweets'] != "be"]
+df = df[df['tweets'] != "you"]
+df = df[df['tweets'] != "are"]
+df = df[df['tweets'] != "it"]
+df = df[df['tweets'] != "with"]
+df = df[df['tweets'] != ""]
+df = df[df['tweets'] != "ownership."]
+df = df[df['tweets'] != "ownership,"]
+df = df[df['tweets'] != "on"]
 
-wordcloud = WordCloud().generate_from_frequencies(lists)
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
+plt.bar(df['tweets'][1:16], df['size'][1:16])
+plt.title("Word count: Republican tweets")
+plt.ylabel("frequency")
 plt.show()
-
-
 
 democ = democ['tweets'].str.split(' ')
 democ = democ.explode('tweets')
@@ -61,17 +92,33 @@ df = pd.DataFrame()
 df['tweets'] = democ
 df['tweets'] = df['tweets'].str.replace("\n", "")
 df['tweets'] = df['tweets'].str.replace("\"", "")
-df['tweets'] = df['tweets'].str.replace("to", "")
-df['tweets'] = df['tweets'].str.replace("the", "")
-df['tweets'] = df['tweets'].str.replace("a", "")
-df['tweets'] = df['tweets'].str.replace("of", "")
-df['tweets'] = df['tweets'].str.replace("is", "")
+
 df = df.groupby(['tweets']).size().reset_index(name="size")
 df = df.sort_values('size', ascending=False)
 
-lists = df.set_index("tweets").to_dict()['size']
+df = df[df['tweets'] != "to"]
+df = df[df['tweets'] != "the"]
+df = df[df['tweets'] != "of"]
+df = df[df['tweets'] != "is"]
+df = df[df['tweets'] != "in"]
+df = df[df['tweets'] != "that"]
+df = df[df['tweets'] != "I"]
+df = df[df['tweets'] != "a"]
+df = df[df['tweets'] != "."]
+df = df[df['tweets'] != "The"]
+df = df[df['tweets'] != "and"]
+df = df[df['tweets'] != "for"]
+df = df[df['tweets'] != "be"]
+df = df[df['tweets'] != "you"]
+df = df[df['tweets'] != "are"]
+df = df[df['tweets'] != "it"]
+df = df[df['tweets'] != "with"]
+df = df[df['tweets'] != ""]
+df = df[df['tweets'] != "ownership."]
+df = df[df['tweets'] != "ownership,"]
+df = df[df['tweets'] != "on"]
 
-wordcloud = WordCloud().generate_from_frequencies(lists)
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
+plt.bar(df['tweets'][1:16], df['size'][1:16])
+plt.title("Word count: Democratic tweets")
+plt.ylabel("frequency")
 plt.show()
